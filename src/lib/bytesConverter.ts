199b802ed70d7feabe58258d12db4b3dbc4947d3
yuri_rodnov <somewaystodo@yandex.ru>
2026-07-19 21:09:37 +0300

    add bytes converteк

--- src/lib/bytesConverter.ts
+++ src/lib/bytesConverter.ts
@@ -1,3 +1,11 @@
-export const convertBytes = (bytes: number, unit: string): number => {
-  return 0;
+export const convertBytes = (bytes: number, decimals: number = 2): string => {
+  if (bytes === 0) return "0 bytes";
+
+  const unit = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(unit));
+
+  return `${parseFloat((bytes / Math.pow(unit, i)).toFixed(dm))} ${sizes[i]}`;
 };
